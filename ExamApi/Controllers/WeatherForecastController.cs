56ebd6603b1ac525f91a201ef7999eedb150e12a
YH <O@Y.H>
2022-07-20 21:40:02 +0900

    tes01

--- ExamApi/Controllers/WeatherForecastController.cs
+++ ExamApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ExamApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test001"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
